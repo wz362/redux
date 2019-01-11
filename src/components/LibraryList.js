97fbbacfbf848e35a6af26019b35aab0b7733354
Wenjia Zhang <wenjiazhang@OLBBs-MacBook-Pro-3.local>
2019-01-11 17:30:54 -0500

    Display the list

---
+++ src/components/LibraryList.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { FlatList } from 'react-native';
+import ListItem from './ListItem';
+
+class LibraryList extends Component {
+    renderItem(library) {
+        return  <ListItem library={library} />;
+    }
+
+    render() {
+        console.log(this.props);
+        return (
+            <FlatList
+                data={this.props.libraries}
+                renderItem={({item}) => this.renderItem(item)}
+                keyExtractor={library => library.id}
+            />
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return { libraries: state.libraries };
+};
+
+export default connect(mapStateToProps)(LibraryList);
